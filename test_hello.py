b851de045c01d0a59e1331b5a0e443d5d278fbe7
r-tamura <rtamura30@gmail.com>
2021-06-30 15:44:13 +0900

    context内でmockするように
    - エラーにはならない
    - 別の警告が発生する

--- test_hello.py
+++ test_hello.py
@@ -1,8 +1,12 @@
 from unittest.mock import MagicMock, Mock
 
 import pytest
+from flask import Flask
 from pytest_mock import MockerFixture
 
+import hello
+
+app = Flask(__name__)
 
 @pytest.fixture
 def flask_request_mock(mocker: MockerFixture) -> MagicMock:
@@ -15,9 +19,21 @@ def flask_request_mock(mocker: MockerFixture) -> MagicMock:
     #
     # ERROR test_hello.py::test_helloworld - RuntimeError: Working outside of request context.
     # が発生する
-    mock = mocker.patch("hello.request")
-    return mock
+    # mock = mocker.patch("hello.request")
+    # return mock
+
+    # OK
+    # RuntimeWarning: coroutine 'AsyncMockMixin._execute_mock_call' was never awaited
+    # のエラーが出力される
+    with app.test_request_context():
+        mock = mocker.patch("hello.request")
+        yield mock
 
 
 def test_helloworld(flask_request_mock):
+    flask_request_mock.get_json.return_value = None
+    print(type(flask_request_mock.get_json))
+
+    hello.hello_world()
+
     assert 1 == 1
